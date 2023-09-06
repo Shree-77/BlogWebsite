58580e61d3421228e7d2599ce14ce18d87fd3aec
shree-77 <shreedhoni7@gmail.com>
2023-09-06 22:09:43 +0530

    get,post,delete added

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog')
+const Blog = require('./models/blog');
+const { render } = require('ejs');
 
 //Express app
 
@@ -13,54 +14,83 @@ const dbURI = 'mongodb+srv://Shree:cB84gmaAEWQyniX6@cluster0.xlxclpj.mongodb.net
 mongoose.connect(dbURI).
 then((result)=>app.listen(8080)).catch((err)=>{
     console.log(err);
-})
-
+});
 
 //Register view Engine
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
-
-//middleware & Static files
-
+// middleware & static files
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-
-
-
-app.get('/',(req, res)=>{
-    res.redirect('/blogs');
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
 });
 
-app.get('/blogs',(req,res)=>{
-   Blog.find().sort({createdAt : -1})
-   .then((result)=>{
-    res.render('index',{title:'All Blogs',blogs:result})
-   }).catch(err=>{
-    console.log(err);
-   })
+// routes
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
 });
 
-app.post('/blogs',(req,res)=>{
-        const blog= new Blog(req.body);
-    blog.save()
-    .then((result)=>{
-        res.redirect('/blogs');
-    }).catch(err=>{console.log(err)});
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
 });
 
-app.get('/about',(req, res)=>{
-  res.render('about',{title:'About'});
+// blog routes
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
 });
 
-app.get('/blogs/create',(req,res)=>{
-    res.render('create',{title:'Create'});
-})
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then(result => {
+      res.render('index', { blogs: result, title: 'All blogs' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
-//404 Page
-app.use((req,res)=>{
-    res.render('404',{title:'404 Not Found'});
-})
+app.post('/blogs', (req, res) => {
+  // console.log(req.body);
+  const blog = new Blog(req.body);
 
+  blog.save()
+    .then(result => {
+      res.redirect('/blogs');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then(result => {
+      res.render('details', { blog: result, title: 'Blog Details' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({ redirect: '/blogs' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
 
