7225fbf7a59735a4c94b45291cb71d29dd5e1edd
shree-77 <shreedhoni7@gmail.com>
2023-11-08 01:33:02 +0530

    nav bar updated

--- app.js
+++ app.js
@@ -1,43 +1,49 @@
 const express = require('express');
-const session = require("express-session");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const blogRoutes = require('./routes/blogroutes');
-const User = require('./models/auth')
-const bcrypt=require("bcryptjs");
+const User = require('./models/auth');
+const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
-//Express app
-
+// Express app
 const app = express();
 
-//connect to database
-
-const dbURI = process.env.MONGODB_URI;  //Blogs
-mongoose.connect(dbURI).
-then((result)=>app.listen(8080)).catch((err)=>{
+// Connect to the database
+const dbURI = process.env.MONGODB_URI; // Blogs
+mongoose.connect(dbURI)
+  .then((result) => app.listen(8080))
+  .catch((err) => {
     console.log(err);
-});
+  });
 
-
-//Register view Engine
-
+// Register view engine
 app.set('view engine', 'ejs');
 
-//Authentication using passport middleware
+// Authentication using passport middleware
+app.use(session({
+  secret: 'cats',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 3600000 // Set the session timeout to 1 hour (in milliseconds)
+  }
+}));
 
-app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-// middleware & static files
+// Middleware & static files
 app.use(express.static('public'));
 app.use(morgan('dev'));
 app.use((req, res, next) => {
   res.locals.path = req.path;
+  res.locals.user = req.user; // Add this line to make the 'user' available in templates
   next();
 });
 
@@ -47,21 +53,19 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Internal Server Error' });
 });
 
-
 // Configure passport
-
 configurePassport();
 
-// routes
+// Routes
 app.get('/', (req, res) => {
   res.redirect('/signup');
 });
 
-//Login 
-app.post("/log-in",
-  passport.authenticate("local", {
-    successRedirect: "/blogs",
-    failureRedirect: "/signup"
+// Login
+app.post('/log-in',
+  passport.authenticate('local', {
+    successRedirect: '/blogs',
+    failureRedirect: '/signup',
   })
 );
 
@@ -69,31 +73,29 @@ app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
 });
 
-//blog routes
-
-app.use('/',blogRoutes);
+// Blog routes
+app.use('/', blogRoutes);
 
 // 404 page
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' });
 });
 
-
-//Configuring Passport for LocalStrategy
+// Configuring Passport for LocalStrategy
 function configurePassport() {
   passport.use(
     new LocalStrategy(async (username, password, done) => {
       try {
         const user = await User.findOne({ username: username });
         if (!user) {
-          return done(null, false, { message: "Incorrect username" });
+          return done(null, false, { message: 'Incorrect username' });
         }
 
         // Use bcrypt to compare the provided password with the hashed password in the database
         const match = await bcrypt.compare(password, user.password);
 
         if (!match) {
-          return done(null, false, { message: "Incorrect password" });
+          return done(null, false, { message: 'Incorrect password' });
         }
 
         return done(null, user);
